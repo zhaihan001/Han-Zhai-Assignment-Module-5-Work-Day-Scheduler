08bef53ecfabd6b570f6cb20ee63d7644376a26a
Han Zhai <zhaihan001@gmail.com>
2022-07-22 01:32:05 -0700

    html layout finished, will start building js

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-$("#currentDay").text(moment().format("dddd, MMMM DD, YYYY"));
+var currentDateTime = moment().format("dddd, MMMM DD, YYYY");
+var saveBtnEl = jQuery.makeArray($('.saveBtn'));
+var textEl = jQuery.makeArray($('textarea'));
+$("#currentDay").text(currentDateTime);
 
+
+function saveTask(){
+    console.log(saveBtnEl);
+    console.log(textEl);
+}
+
+$(".saveBtn").click(saveTask);
