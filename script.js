9ea15696970161beb160fc713528c5af6b482fd5
Han Zhai <zhaihan001@gmail.com>
2022-07-22 02:10:42 -0700

    Successfully added js code to set textarea background color based of current time

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 var currentDateTime = moment().format("dddd, MMMM DD, YYYY");
-var saveBtnEl = jQuery.makeArray($('.saveBtn'));
-var textEl = jQuery.makeArray($('textarea'));
+var currentHour = moment().format("H");
+console.log(currentHour);
+var saveBtnEl = jQuery.makeArray($(".saveBtn"));
+var textEl = jQuery.makeArray($("textarea"));
+
+// Display current Date in header
 $("#currentDay").text(currentDateTime);
 
 
@@ -9,4 +13,15 @@ function saveTask(){
     console.log(textEl);
 }
 
+// Click to run function to save the textarea 
 $(".saveBtn").click(saveTask);
+
+
+// Set different textarea background color based of current time.
+for ( let i = 0; i < textEl.length; i++){
+    if ( (i+9) > currentHour){
+        textEl[i].style.backgroundColor = "rgb(95, 206, 132)";
+    } else if ((i+9)===currentHour){
+        textEl[i].style.backgroundColor = "rgb(222, 90, 37)";
+    }
+}
