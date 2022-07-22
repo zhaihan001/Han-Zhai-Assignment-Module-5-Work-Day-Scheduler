d0d62c30c87e267959b8414cb3002188d2e7fa41
Han Zhai <zhaihan001@gmail.com>
2022-07-22 16:58:33 -0700

    rewrite a JS function using Jquery for practice

--- script.js
+++ script.js
@@ -9,15 +9,26 @@ function init() {
     // Display current Date in header
     $("#currentDay").text(currentDateTime);
 
-    // Set different textarea background color based of current time.
-    for (let i = 0; i < textEl.length; i++) {
-        if ((i + 9) > currentHour) {
-            textEl[i].style.backgroundColor = "rgb(95, 206, 132)";
-        } else if ((i + 9) == currentHour) {
-            textEl[i].style.backgroundColor = "rgb(222, 90, 37)";
+    // Same function as below. Try to practice using JQuery as mu as I can. 
+    // JQuery - Set textarea background color based off current time.
+    $("textarea").each(function(index){
+        if ((index + 9) > currentHour) {
+            $(this).css("background-color","rgb(95, 206, 132)")
+        } else if ((index + 9) == currentHour) {
+            $(this).css("background-color","rgb(222, 90, 37)")
         }
-    }
-    // Get localstorage and display the value on webpage
+    })
+
+    // JS - Set textarea background color based off current time.
+    // for (let i = 0; i < textEl.length; i++) {
+    //     if ((i + 9) > currentHour) {
+    //         textEl[i].style.backgroundColor = "rgb(95, 206, 132)";
+    //     } else if ((i + 9) == currentHour) {
+    //         textEl[i].style.backgroundColor = "rgb(222, 90, 37)";
+    //     }
+    // }
+
+    // JS - render localstorage to display
     for (let k = 0; k < localStorage.length; k++){
         var taskRender = localStorage.getItem("Task"+[k]);
         textEl[k].value = taskRender;
@@ -29,7 +40,6 @@ function init() {
 function saveTask(event) {
     event.preventDefault();
     for (let j = 0; j < textEl.length; j++) {
-
         textInput = textEl[j].value;
         localStorage.setItem("Task" + [j], textInput);
     }
